0fb20b868f15c4d7661f3443315b813feb84154e
dhdzmota <dhdzmota@gmail.com>
2023-11-05 21:43:33 -0600

    Included a whole set of general functions to process all the information and develop an explainable SOM.

--- general_pipeline.py
+++ general_pipeline.py
@@ -2,7 +2,8 @@ import subprocess
 
 command_list = [
     'python src/data/download_data.py',
-    'python src/data/clean_data.py',
+    'python src/data/process_data.py',
+    'python src/data/prepare_data.py',
 ]
 
 for command in command_list:
--- src/data/download_data.py
+++ src/data/download_data.py
@@ -1,46 +1,9 @@
-import requests
-from bs4 import BeautifulSoup
 import pandas as pd
 import os
 
-
-SPLIT_VAL = '<tr><td  align=right>'
-HTML_TAG1 = 'a'
-HTML_TAG2 = 'td'
-MIN_ATOMIC_NB = 1
-MAX_ATOMIC_NB = 118
-ZFILL_VAL = 3
-URL_LINK = 'https://periodictable.com/Elements/{}/data.html'
-
-
-def get_element_data_from_url(url):
-    element_information = {}
-    url_info = requests.get(url)
-    # To know which elements, an inspection to the desired url must be done.
-    url_info_tag = url_info.text.split(SPLIT_VAL)
-    for tag_inf in url_info_tag:
-        soup = BeautifulSoup(tag_inf, features="html.parser")
-        element_property = soup.find(HTML_TAG1).text
-        element_value = soup.find(HTML_TAG2).text
-        element_information[element_property] = element_value
-    return element_information
-
-
-def fill_periodic_table_dictionary():
-    periodic_table = {}
-    for element in range(MIN_ATOMIC_NB, MAX_ATOMIC_NB+1):
-        element_nb = str(element).zfill(ZFILL_VAL)
-        link = URL_LINK.format(element_nb)
-        periodic_table[f'{element_nb}'] = get_element_data_from_url(link)
-    return periodic_table
-
-
-def transform_periodic_table_dict_to_df(periodic_table_dict):
-    periodic_table_df_t = pd.DataFrame(periodic_table_dict)
-    # Non usable column
-    periodic_table_df_t.drop('H', inplace=True)
-    periodic_table_df = periodic_table_df_t.T
-    return periodic_table_df
+URL_LINK = 'https://pubchem.ncbi.nlm.nih.gov' \
+            '/rest/pug/periodictable/' \
+            'CSV?response_type=save&response_basename=PubChemElements_all'
 
 
 def save_as_csv(path, df):
@@ -56,14 +19,15 @@ def read_data_as_csv(path, keep_na=False):
     return df
 
 
+def get_periodic_table(url):
+    periodic_table_df = read_data_as_csv(url, keep_na=True)
+    return periodic_table_df
+
+
 if __name__ == '__main__':
     file_path = os.path.dirname(os.path.abspath(__file__))
     general_path = os.path.join(file_path, '..', '../')
     data_raw_path = os.path.join(general_path, 'data', 'raw')
-    data_raw_filename = os.path.join(data_raw_path, 'data.csv')
-
-    periodic_table_dict = fill_periodic_table_dictionary()
-    data = transform_periodic_table_dict_to_df(
-        periodic_table_dict=periodic_table_dict
-    )
+    data_raw_filename = os.path.join(data_raw_path, 'periodic_table_data.csv')
+    data = get_periodic_table(URL_LINK)
     save_as_csv(data_raw_filename, data)
